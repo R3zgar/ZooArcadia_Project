7fb31673e377ae4bf3c8523876173e979b9e820c
R3zgar <rzgarevam05@gmail.com>
2024-10-12 03:38:01 +0200

    Création du dossier 'auth' et transfert des pages nécessitant une autorisation. Ajout et modification des pages 'account.html' et 'editPassword.html' avec les ajustements nécessaires pour la réactivité.

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,9 +6,11 @@ export const allRoutes = [
     new Route("/services", "Services", "/pages/services.html"),
     new Route("/habitats", "Habitats", "/pages/habitats.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Inscriotion", "/pages/signup.html"),
     new Route("/confidentialite", "Politique de confidentialité", "/pages/confidentialite.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscriotion", "/pages/auth/signup.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
 
 ];
 
